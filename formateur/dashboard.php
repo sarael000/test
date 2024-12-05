f6fddb7c5c5a455c6093052b35eb55045bab7524
sarael000 <114244825+sarael000@users.noreply.github.com>
2024-12-05 10:28:49 +0100

    Add files via upload

---
+++ formateur/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if ($_SESSION['role'] !== 'formateur') {
+    header("Location: https://sarael000.github.io/test/login.php");
+    exit();
+}
+
+echo "<h1>Bienvenue Formateur</h1>";
+echo "<a href='creer_test.php'>Créer un test</a><br>";
+echo "<a href='consulter_tests.php'>Consulter les tests</a><br>";
+?>
